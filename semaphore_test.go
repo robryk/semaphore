04786e6c9b8a53c6b8d4b45d22d216848f8b777f
Robert Obryk <robryk@gmail.com>
2013-05-30 16:25:27 +0200

    Extended tests.

--- semaphore_test.go
+++ semaphore_test.go
@@ -1,13 +1,15 @@
 package semaphore
 
 import (
+	"sync"
 	"sync/atomic"
 	"testing"
+	"time"
 )
 
 type testSemaphore struct {
 	s     *Semaphore
-	value int64
+	value int64 // value contains an upper bound on the current semaphore's value
 	err   chan string
 }
 
@@ -21,7 +23,6 @@ func (s *testSemaphore) setError(e string) {
 func (s *testSemaphore) Error() string {
 	select {
 	case e := <-s.err:
-		s.err <- e
 		return e
 	default:
 		return ""
@@ -62,8 +63,17 @@ func TestSemaphore(t *testing.T) {
 	const P = 5
 	const N = 1000
 	s := newTestSemaphore(P)
-	done := make(chan struct{}, P)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		for j := 0; j < N; j++ {
+			v := s.Drain()
+			s.Release(v)
+		}
+		wg.Done()
+	}()
 	for i := 0; i < P; i++ {
+		wg.Add(1)
 		go func() {
 			for j := 0; j < N; j++ {
 				s.Acquire(1)
@@ -71,14 +81,85 @@ func TestSemaphore(t *testing.T) {
 				s.Acquire(2)
 				s.Release(2)
 			}
-			done <- struct{}{}
+			wg.Done()
 		}()
 	}
-	for i := 0; i < P; i++ {
-		<-done
-	}
+	wg.Wait()
 	if err := s.Error(); err != "" {
 		t.Error(err)
+	}
+}
+
+func TestDrain(t *testing.T) {
+	const P = 5
+	const N = 1000
+	s := newTestSemaphore(P)
+	var wg sync.WaitGroup
+	for i := 0; i < P; i++ {
+		wg.Add(1)
+		go func() {
+			for j := 0; j < N; j++ {
+				v := s.Drain()
+				s.Release(v)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	if err := s.Error(); err != "" {
+		t.Error(err)
+	}
+}
+
+func TestCancel(t *testing.T) {
+	ch := make(chan struct{})
+	s := New(1)
+	if s.AcquireCancellable(1, ch) != true {
+		t.Fatal("AcquireCancellable spuriously cancelled.")
+	}
+	s.Release(1)
+	go func() {
+		time.Sleep(time.Duration(100) * time.Millisecond)
+		close(ch)
+	}()
+	if s.AcquireCancellable(2, ch) != false {
+		t.Fatal("AcquireCancellable incorrectly succeeded")
+	}
+	if v := s.Drain(); v != 1 {
+		t.Errorf("Drain drained %d units when %d should have been available", v, 1)
+	}
+}
+
+func TestCancelConcurrent(t *testing.T) {
+	const M = 5
+	const N = 5
+	ch := make(chan struct{}, M)
+	for i := 0; i < M; i++ {
+		ch <- struct{}{}
+	}
+
+	results := make(chan bool, M+N)
+
+	s := New(2*N + 1)
+	for i := 0; i < M+N; i++ {
+		go func() {
+			results <- s.AcquireCancellable(2, ch)
+		}()
+	}
+
+	successes := 0
+	for i := 0; i < M+N; i++ {
+		if <-results {
+			successes++
+		}
+	}
+	close(results)
+
+	if successes != N {
+		t.Fatalf("Expected %d AcquireCancellable calls to succeed; instead %d succeeded.", N, successes)
+	}
+	if v := s.Drain(); v != 1 {
+		t.Errorf("Drain drained %d units when %d should have been available", v, 1)
 	}
 }
 
